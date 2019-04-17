2de955f7a823e94e512371556c1695dfd94adabc
Simon Wells <siwells@gmail.com>
2019-04-17 19:01:32 +0100

    Update core tests

--- tools/python/src/test/core_test.py
+++ tools/python/src/test/core_test.py
@@ -3,51 +3,20 @@ import os
 import unittest
 
 import sadface as sf
+#import sadface.config
 
-class TestSADFaceFunctions(unittest.TestCase):
+class TestCore(unittest.TestCase):
     def setUp(self):
-       pass
+        sf.reset()
 
     def tearDown(self):
-        pass
-    
-    def test_init(self):
-        """
-        Tests: sadface.init()
-
-        init() returns a dict containing data, e.g. dates & UUIDs, that can vary on each invokation
-        so the values for these keys are emptied before comparing to a dict loaded from a known
-        good JSON string.
-        """
-        out = sf.init()
-        out['metadata']['core']['created'] = ""
-        out['metadata']['core']['edited'] = ""
-        out['metadata']['core']['id'] = ""
-
-        expected = json.loads('{ "edges": [], "metadata": { "core": { "analyst_email": "user@email.address", "analyst_name": "A User", "created":"", "edited":"", "id":"", "version": "0.2" } }, "nodes": [], "resources": []}')
-        self.assertEqual(out, expected)
-
-    def test_init_with_config(self):
-        """
-        Tests: sadface.init() 
-        
-        Uses a user specified configuration that is different to the default
-        """
-        sf.config.set_config_location("deploy/etc/simon.cfg")
-        sf.config.load()
-        out = sf.init()
-        out['metadata']['core']['created'] = ""
-        out['metadata']['core']['edited'] = ""
-        out['metadata']['core']['id'] = ""
-        expected = json.loads('{ "edges": [], "metadata": { "core": { "analyst_email": "siwells@gmail.com", "analyst_name": "Simon Wells", "created": "", "edited": "", "id": "", "version": "0.2" } }, "nodes": [], "resources": []}')
-        self.assertEqual(out, expected)
-
+        sf.reset()
 
     def test_get_version(self):
         """
         Tests: sadface.get_version()
         """
-        sf.sd = sf.init()
+        sf.init()
         out = sf.get_version();
         expected = "0.2"
         self.assertEqual(out, expected)
