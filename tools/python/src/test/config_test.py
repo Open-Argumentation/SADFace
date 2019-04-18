a781903944e9610499648f08bdebf21bb3f377ca
Simon Wells <siwells@gmail.com>
2019-04-18 11:39:36 +0100

    Tested path must be within the repo

--- tools/python/src/test/config_test.py
+++ tools/python/src/test/config_test.py
@@ -16,9 +16,9 @@ class TestConfig(unittest.TestCase):
         Tests: sadface.config.set_location()
 
         """
-        sf.config.set_location("deploy/etc/test.cfg")
+        sf.config.set_location("etc/defaults.cfg")
         out = sf.config.location
-        expected = "deploy/etc/test.cfg"
+        expected = "etc/defaults.cfg"
         self.assertEqual(out, expected)
 
 class TestLoad(unittest.TestCase):
