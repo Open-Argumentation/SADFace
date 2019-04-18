01e3ef302a2712c90f3dae92672d8647bc845740
Simon Wells <siwells@gmail.com>
2019-04-18 11:38:29 +0100

    Test for unset location handling in config load

--- tools/python/src/test/config_test.py
+++ tools/python/src/test/config_test.py
@@ -21,6 +21,45 @@ class TestConfig(unittest.TestCase):
         expected = "deploy/etc/test.cfg"
         self.assertEqual(out, expected)
 
+class TestLoad(unittest.TestCase):
+    def setUp(self):
+        sf.reset()
+        sf.config.set_location("etc/defaults.cfg")
+
+    def tearDown(self):
+        sf.reset()
+
+    def test_load(self):
+        """
+        Tests: sadface.config.load()
+
+        """
+        sf.config.load()
+        out = sf.config.current.get("analyst", "name")
+        expected = 'A User'
+        self.assertEqual(out, expected)
+
+class TestLoadNoConfig(unittest.TestCase):
+    def setUp(self):
+        sf.reset()
+
+    def tearDown(self):
+        sf.reset()
+    
+    def test_load_no_config(self):
+        """
+        Tests: sadface.config.load()
+
+        Ensure that if load is called but location isn't set then an
+        exception must be raised
+        """
+        with self.assertRaises(Exception) as context:
+            sf.config.load()
+        out = str(context.exception)
+        expected = 'Tried to load config file but location is set to None'
+        self.assertEqual(out, expected)
+
+
 if __name__ == "__main__":
     
     unittest.main()
