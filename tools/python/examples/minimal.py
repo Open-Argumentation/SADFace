7a18527568e4f14b588b8435f394a1c754b5e3ce
Simon Wells <siwells@gmail.com>
2020-06-25 17:34:18 +0100

    Remove inaccurate portion of docstring

--- tools/python/examples/minimal.py
+++ tools/python/examples/minimal.py
@@ -1,7 +1,7 @@
 import sadface as sf
 
 """
-A minimal script to create the bare minimum SADFace document. This includes an edges, nodes, resources, and metadata block. First a default configuration file (default.cfg) is generated and stored in a folder /etc relative to the current working directory. Core metadata for the new SADFace document is then completed according to that default configuration file. The resulting SADFace description is then run through a validation procedure before being pretty printed to the terminal.
+A minimal script to create the bare minimum SADFace document. This includes each of an edges, nodes, resources, and metadata block. The resulting SADFace description is then run through a validation procedure before being pretty printed to the terminal.
 """
 
 sf.init()
