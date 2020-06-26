e72a204cd725efdfd7d96953fe95b60932f31be5
Simon Wells <siwells@gmail.com>
2020-06-26 18:49:44 +0100

    Update minimal example

--- tools/python/examples/minimal.py
+++ tools/python/examples/minimal.py
@@ -4,7 +4,7 @@ import sadface as sf
 A minimal script to create the bare minimum SADFace document. This includes each of an edges, nodes, resources, and metadata block. The resulting SADFace description is then run through a validation procedure before being pretty printed to the terminal.
 """
 
-sf.init()
+sf.initialise()
 result, problems = sf.validation.verify(sf.get_document(), as_string=True)
 
 if result:
