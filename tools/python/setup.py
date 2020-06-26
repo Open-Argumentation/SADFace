1b0e204c12addfa01a4ae17a130c9cba212bd53f
Simon Wells <siwells@gmail.com>
2020-06-26 18:39:26 +0100

    Add license, readme, and long descrition keys for pip packaging

--- tools/python/setup.py
+++ tools/python/setup.py
@@ -2,9 +2,17 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name='sadface',
     description='SADFace - The Simple Argument Description Format',
+    long_description=readme,
+    license=license,
     author='Simon Wells',
     license='GNU GPL3',
     url='https://github.com/ARG-ENU/SADFace.git',
