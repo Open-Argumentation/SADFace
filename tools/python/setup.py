d8f185d8bda19f76632321638c437fc1a479b98b
Simon Wells <siwells@gmail.com>
2021-01-04 17:51:06 +0000

    Fix for pip distribution

--- tools/python/setup.py
+++ tools/python/setup.py
@@ -2,22 +2,19 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
+with open('README.md', "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 exec(open('sadface/version.py').read())
 
 setup(
     name='sadface',
     description='SADFace - The Simple Argument Description Format',
-    long_description=readme,
-    license=license,
+	long_description_content_type='text/markdown',
+    long_description=long_description,
     author='Simon Wells',
-    url='https://github.com/ARG-ENU/SADFace.git',
+    url='https://github.com/Open-Argumentation/SADFace.git',
     author_email='mail@simonwells.org',
 	version=__version__,
-    packages=find_packages(exclude=('deploy', 'etc', 'examples'))
+    packages=find_packages(exclude=('deploy', 'etc', 'examples')),
 )
