bfffc9f00c27753a2d795d14834abceba3f9b4f8
Simon Wells <siwells@gmail.com>
2021-01-04 17:51:56 +0000

    Add missing set_doc call so lib is aware of the loaded file

--- tools/python/examples/loader.py
+++ tools/python/examples/loader.py
@@ -5,6 +5,7 @@ A minimal script to demonstrate loading and verifying a SADFace document.
 """
 
 sfdoc = sf.load_from_file("minimal.json")
+sf.set_doc(sfdoc)
 
 result, problems = sf.validation.verify(sf.get_document())
 if result:
